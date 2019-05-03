b00e42c951baec2267541d49ea842577ba089408
Denys Zhorov <2013dvl@gmail.com>
2019-05-03 20:07:23 +0200

    structured the project and added an app and upgraded the index.js

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import TodoListItem from './todo-list-item';
+import TodoListItem from '../todo-list-item';
+import './todo-list';
+
+
 const TodoList = ({todos}) => {
 
 const elements = todos.map((item) => {
@@ -7,14 +10,14 @@ const elements = todos.map((item) => {
     const { id, ...itemProps } = item
 
    return (
-        <li key = { id}>
+        <li key = { id} className="list-group-item">
         <TodoListItem {... itemProps}/>
         </li>
     );
 }) 
 
 return (
-        <ul>
+        <ul className="list-group todo-list">
           {elements}
         </ul>
         );
--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
 
+import App from './components/app'
 
-const App = () =>{
-
- const todoData = [
-     {label : 'Drink Coffee', important: false, id: 1},
-     {label : 'Make Awesome App', important: true, id: 2},
-     {label : 'Start React App', important: false, id: 3},
-     {label : 'Have a lunch', important: true, id: 4}
- ]
-
-    return(
-    <div>
-        <AppHeader/>
-        <SearchPanel/>
-       <TodoList todos={todoData}/>
-    </div>
-);};
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
